b237bdcf2f3bcbe1855cb549eea011b743b8097f
PavelMikhailov <n9032692530@gmail.com>
2022-11-18 18:24:50 +0300

    Initial commit

---
+++ seleck.py
@@ -0,0 +1,8 @@
+def start():
+    while True:
+        func = input('Для импорта нажмите "1", для экспорта нажмите "2":')
+        if fun == '1':
+            imp_data(func)
+        elif func == '2':
+            exp_data(func)
+        break
