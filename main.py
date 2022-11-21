218bea234dda3e129e218c9ef378410585e1ebe5
PavelMikhailov <n9032692530@gmail.com>
2022-11-22 02:52:25 +0300

    Работа с файлом CSV Телефонная книга

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
-from seleck import start
-import csv
+# ''' Задание в группах: Создать телефонный справочник с возможностью импорта и экспорта данных в нескольких форматах
+#     (CSV, JSON, XML).
+# '''
+from help import help
+from log import log
+from create_phonebook_csv import create_phonebook_csv
+from write import write
+from read import read
+from delete import delete
+from export_json import df_json
 
-data = [['Фамилие', 'Имя', 'Отчество', 'Телефон'],
-        ['Нестеров', 'Сергей', 'Викторович', '89031234567'],
-        ['Андрианив', 'Олег', 'Валерьевич', '89161234567'],
-        ['Щеглов', 'Андрей', 'Иванович', '89017654321'],
-        ['Корольков', 'Денис', 'Александрович', '89037654321']]
+# Ф-ция создаст телефонную книгу 'Phonebook.csv' если ее нет
+create_phonebook_csv()
 
+while (True):
+    print(
+        'Для просмотра инструкции по работе с командной строкой введите команду "СПРАВКА" (регистр символов не важен).')
+    command = input('Командная строка: ')
+    if command.lower() == 'справка':
+        help()
+        log('Вызов "СПРАВКИ"', 'ОК!')
+        print('-' * 10)
+    elif command.lower() == 'добавить':
+        command = write()
+        txt = 'ОК!' if command else 'ERROR!'
+        log('Создание нового контакта', txt)
+        print('-' * 10)
+    elif command.lower() == 'найти':
+        command = read()
+        txt = 'ОК!' if command else 'ERROR!'
+        log('Поиск контактов', txt)
+        print('-' * 10)
+    elif command.lower() == 'удалить':
+        command = delete()
+        txt = 'ОК!' if command else 'ERROR!'
+        log('Удаление контактов', txt)
+        print('-' * 10)
+    elif command.lower() == 'json':
+        command = df_json()
+        txt = 'ОК!' if command else 'ERROR!'
+        log('Преобразование в JSON', txt)
+        print('-' * 10)
 
-with open('sw_data_new.csv', 'w') as f:
-    writer = csv.writer(f)
-    for row in data:
-        writer.writerow(row)
+        '''
+    elif command.lower() == 'import_csv':
+    elif command.lower() == 'import_xml':
+    elif command.lower() == 'import_json':
 
-with open('sw_data_new.csv') as f:
-    print(f.read())
-start()
-
+    elif command.lower() == 'export_csv':
+    elif command.lower() == 'export_json':
+    elif command.lower() == 'export_xml':
+'''
+    else:
+        print('Такой команды нет')
--- seleck.py
+++
@@ -1,8 +0,0 @@
-def start():
-    while True:
-        func = input('Для импорта нажмите "1", для экспорта нажмите "2":')
-        if func == '1':
-            imp_data()
-        elif func == '2':
-            exp_data()
-        break
